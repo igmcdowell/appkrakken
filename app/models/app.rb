0a11d3eecc7a375aae3ec1bfff8b8719f6bb7c53
Ian McDowell <igmcdowell@gmail.com>
2011-12-19 20:50:15 -0800

    consolidated update_details, moved into model, and did some query tuning

--- app/models/app.rb
+++ app/models/app.rb
@@ -9,23 +9,77 @@ class App < ActiveRecord::Base
   after_create :add_price_history
   after_save :check_price_change
   
+
+  
   def add_price_history
-    if self.prices.length > 0 
+    if prices.length > 0 
       oldp = self.old_price
       oldp.end_date = DateTime.current()
       oldp.save
     end
     self.prices.create(price: self.price, start_date:DateTime.current())
-    #note: at this point a query for a.prices.first will return old_price, instead of the new one, since it hits the cache. Not sure how to force it to issue a fresh query. May only matter in the console
+    #note: self. this point self.query for self.prices.first will return old_price, instead of the new one, since it hits the cache. Not sure how to force it to issue self.fresh query. May only matter in the console
   end
   
   def check_price_change
-    if self.prices.length > 0 and (self.prices.first.price != self.price)
-      add_price_history
+    if prices.length > 0 and (prices.first.price != price)
+     add_price_history
     end
   end
   
   def old_price
-    self.prices.first
+    prices.first
+  end
+
+  # I could use find_or_create_by_<attr>, but that generates a separate sql query for each candidate. In the most common case (no update), that's going to generate a mess of extra sql queries. Update many relation does 1 query then checks it against candidates and only creates/destroys as necessary.
+  def update_many_relation(rel, identifier, candidates)
+    existing_relations = eval("self.#{rel}")
+    existing_ids = existing_relations.map {| trel | eval("trel.#{identifier}")}.compact
+    new_ids = candidates-existing_ids
+    deleted_ids = existing_ids - candidates
+    new_ids.each do | val |
+      eval("#{rel}.create(#{identifier}: '#{val}')")
+    end
+    deleted_ids.each do | val |
+      eval(  "#{rel}.where(#{identifier}: '#{val}')[0].destroy"  )
+    end
+  end
+  
+  def update_details(details)
+    require 'net/http'
+    require 'uri'
+    details = Net::HTTP.get URI.parse('http://itunes.apple.com/lookup?id='+app_id.to_s)
+    details = ActiveSupport::JSON.decode(details)["results"][0]
+    #note: I'm unable to determine which attributes can only change on a resubmission. 
+    #since I've already pulled all of them in the search result, it's easiest just to remap everything.
+    self.price, self.description = details["price"], details["description"]
+    self.rating, self.numratings  = details["averageUserRating"], details["userRatingCount"]
+    self.game_url, self.age_rating, self.game_center, self.artwork60, self.artwork100 = details["trackViewUrl"], details["contentAdvisoryRating"], details["isGameCenterEnabled"], details["artworkUrl60"], details["artworkUrl100"]
+    self.dev_id, self.dev_site, self.dev_name  = details["artistId"], details["artistViewUrl"], details["artistName"]
+    self.primary_genre_name, self.primary_genre_id = details["primaryGenreName"], details["primaryGenreId"]
+    self.size, self.version, self.release_notes, self.release_date = details["fileSizeBytes"], details["version"], details["releaseNotes"], details["releaseDate"]
+    genres, genre_codes, ipad_screenshot_urls, language_codes, screenshot_urls, supported_devices = details["genres"], details["genreIds"], details["iPadScreenshotUrls"], details["languageCodesISO2A"], details["screenshot_urls"], details["supportedDevices"]
+    self.save
+    #The has-many relations below require sql queries. Create new relations attempts to tune it a little.
+    if genres
+      self.update_many_relation('genres', 'name', genres)
+    end
+    if genre_codes
+      genre_codes = genre_codes.map { | gc | gc.to_i}
+      self.update_many_relation('genre_codes', 'genre', genre_codes)
+    end
+    if ipad_screenshot_urls
+      self.update_many_relation('ipad_screenshot_urls', 'url', ipad_screenshot_urls)
+    end
+    if language_codes
+      self.update_many_relation('language_codes', 'language', language_codes)
+    end
+    if screenshot_urls
+      self.update_many_relation('screenshot_urls', 'url', screenshot_urls)
+    end
+    if supported_devices
+      self.update_many_relation('supported_devices', 'device', supported_devices)
+    end
+
   end
 end
--- lib/tasks/my_rakes.rake
+++ lib/tasks/my_rakes.rake
@@ -7,66 +7,6 @@
 # game codes: 7001, 7002, 7003, 7004, 7005, 7006, 7007, 7008, 7009, 7010, 7011, 7012, 7013, 7014, 7015, 7016, 7017, 7018, 7019"
 
 # format for search API: http://itunes.apple.com/lookup?id=322422566
-require 'net/http'
-require 'uri'
-
-def get_feed(ipad, free, limit, genre)
-  padstr = ['','ipad']
-  freestr = ['paid', 'free']
-  uri = "http://itunes.apple.com/us/rss/top#{freestr[free]}#{padstr[ipad]}applications/limit=#{limit.to_s}/genre-#{genre.to_s}/json"
-  feed = Net::HTTP.get URI.parse(uri)
-  json = ActiveSupport::JSON.decode(feed)["feed"]
-end
-
-def fetch_details(app_id)
-  details = Net::HTTP.get URI.parse('http://itunes.apple.com/lookup?id='+app_id)
-  details = ActiveSupport::JSON.decode(details)["results"][0]
-end
-
-def map_details(details, a)
-  price, description = details["price"], details["description"]
-  rating, numratings  = details["averageUserRating"], details["userRatingCount"]
-  game_url, age_rating, game_center, artwork60, artwork100 = details["trackViewUrl"], details["contentAdvisoryRating"], details["isGameCenterEnabled"], details["artworkUrl60"], details["artworkUrl100"]
-  dev_id, dev_site, dev_name  = details["artistId"], details["artistViewUrl"], details["artistName"]
-  primary_genre_name, primary_genre_id = details["primaryGenreName"], details["primaryGenreId"]
-  size, version, release_notes, release_date = details["fileSizeBytes"], details["version"], details["releaseNotes"], details["releaseDate"]
-    a.price, a.description, a.rating, a.numratings, a.game_url, a.age_rating, a.game_center, a.artwork60, a.artwork100, a.dev_id, a.dev_site, a.dev_name, a.primary_genre_name, a.primary_genre_id, a.size, a.version, a.release_notes, a.release_date = price, description, rating, numratings, game_url, age_rating, game_center, artwork60, artwork100, dev_id, dev_site, dev_name, primary_genre_name, primary_genre_id, size, version, release_notes, release_date
-  
-  genres, genre_codes, ipad_screenshot_urls, language_codes, screenshot_urls, supported_devices = details["genres"], details["genreIds"], details["iPadScreenshotUrls"], details["languageCodesISO2A"], details["screenshot_urls"], details["supportedDevices"]
-
-  if genres
-    genres.each do | genre | 
-      a.genres.find_or_create_by_name(name: genre)
-    end
-  end
-  if genre_codes
-    genre_codes.each do | id | 
-      a.genre_codes.find_or_create_by_genre(genre: id)
-    end
-  end
-  if ipad_screenshot_urls
-    ipad_screenshot_urls.each do | url |
-      a.ipad_screenshot_urls.find_or_create_by_url(url: url)
-    end
-  end
-  if language_codes
-    language_codes.each do | code |
-      a.language_codes.find_or_create_by_language(language: code)
-    end
-  end
-  if screenshot_urls
-    screenshot_urls.each do | url |
-      a.screenshot_urls.find_or_create_by_url(url: url)
-    end
-  end
-  if supported_devices
-    supported_devices.each do | device |
-      a.supported_devices.find_or_create_by_device(device: device)
-    end
-  end
-
-  return a
-end
 
 desc "Import apps." 
   task :import_apps => :environment do
@@ -81,10 +21,9 @@ desc "Import apps."
       url = entry["id"]["label"]
       idstring = url.split(/\//).last.split(/\?/).first
       app_id = idstring[2,idstring.length]
-      details = fetch_details(app_id)
       name, description, price, app_id = entry["im:name"]["label"], entry["summary"], entry["im:price"]["attributes"]["amount"], app_id
       a = App.find_or_create_by_app_id(:app_id => app_id, :name => name, :description => description, :price => price)
-      a = map_details(details, a)
+      a.update_details
       a.save
     end
   end
